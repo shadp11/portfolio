eb93a07141162f8257b74ef58e1393925cd71bf1
Kaysha Dela Pena <kayshadelapena1230@gmail.com>
2026-07-19 19:53:47 +0800

    Fix dependency lock file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { motion } from "framer-motion";
 import {
   AcademicCapIcon,
-  ArrowDownCircleIcon,
+  ArrowDownCircleIcon,  
   ArrowTopRightOnSquareIcon,
   BoltIcon,
   BriefcaseIcon,
